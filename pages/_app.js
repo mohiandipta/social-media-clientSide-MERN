bd7782af401878e5fab7f4e5c21e8d02852d838b
mohiandipta <mohianmostofa@gmail.com>
2021-11-11 21:12:10 +0600

    context state added

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,18 @@ import Head from "next/head";
 import 'react-toastify/dist/ReactToastify.css';
 import "antd/dist/antd.css"
 import { ToastContainer } from 'react-toastify';
+import { UserProvider } from "../context";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <>
+        <UserProvider>
             <ToastContainer position="top-center" />
             <Nav />
             <Head>
                 <link rel="stylesheet" type="text/css" href="/css/style.css" />
             </Head>
             <Component {...pageProps} />
-        </>
+        </UserProvider>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../context';
 
 const Home = () => {
-    return (
 
+    const [state, setState] = useContext(UserContext)
+
+    return (
         <div className="">
             <div>
                 <h1 className="display-1 text-center py-5">Home Page</h1>
             </div>
             <img src="/images/bg.png" alt="image" />
+            {JSON.stringify(state)}
         </div>
     );
 }
