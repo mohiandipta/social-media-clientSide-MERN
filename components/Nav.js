17c85e36429134399dd7823a9d20b949bdc6fdfa
mohiandipta <mohianmostofa@gmail.com>
2021-11-13 22:36:02 +0600

    added logout in the nav and its working perfectly

--- components/Nav.js
+++ components/Nav.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import Link from "next/link";
+import { useContext } from 'react';
+import { UserContext } from '../context';
+import { useRouter } from 'next/dist/client/router';
 
 const Nav = () => {
+    const router = useRouter()
+
+    const [state, setstate] = useContext(UserContext);
+    const logout = () => {
+        // remove local storage data
+        window.localStorage.removeItem("auth")
+        // making object empty
+        setstate(null)
+        // routing to the login page
+        router.push("/login")
+    }
+
     return (
-        <nav className="nav bg-white d-flex justify-content-center" style={{ height: "70px", alignContent: 'center', boxShadow: "0px 0px 7px #737373" }}>
+        <nav className="nav d-flex bg-white d-flex justify-content-center" style={{ height: "70px", alignContent: 'center', boxShadow: "0px 0px 7px #737373" }}>
             <Link href="/">
                 <a className="nav-link text-dark">Home</a>
             </Link>
+
             <Link href="/about">
                 <a className="nav-link text-dark">About</a>
             </Link>
+
             <Link href="/login">
                 <a className="nav-link text-dark">Login</a>
             </Link>
+
             <Link href="/register">
                 <a className="nav-link text-dark">Register</a>
             </Link>
-        </nav>
+
+
+            <a onClick={logout} className="nav-link text-dark" > Logout</a >
+        </nav >
     );
 }
 
