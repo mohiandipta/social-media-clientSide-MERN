4380aef3b2916f11e69ea422dbe19db19f8bbfd8
mohiandipta <mohianmostofa@gmail.com>
2021-12-01 23:57:10 +0600

    wrapper component for authorization started

---
+++ components/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/dist/client/router";
+import { SyncOutlined } from "@ant-design/icons"
+import { Default } from "react-toastify/dist/utils";
+
+const UserRouter = ({ children }) => {
+
+}
+
+export default UserRouter
