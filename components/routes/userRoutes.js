6c3abdda5e15dc7a4c38165987ca291f13b5996b
mohiandipta <mohianmostofa@gmail.com>
2021-12-05 21:25:28 +0600

    UserRouter is a wrapper component

--- components/routes/userRoutes.js
+++ components/routes/userRoutes.js
@@ -5,7 +5,25 @@ import { SyncOutlined } from "@ant-design/icons"
 import { Default } from "react-toastify/dist/utils";
 
 const UserRouter = ({ children }) => {
+    const [ok, setOk] = useState(false);
+    const route = UserRouter();
 
+    useEffect(() => {
+        getCorrentUser()
+    }, []);
+
+    const getCorrentUser = async () => {
+        try {
+            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/current-user`)
+            if (data.ok) {
+                setOk(true)
+            }
+        } catch (error) {
+            route.push('/login')
+        }
+    }
+
+    return !ok ? (<SyncOutlined spin className="d-flex justify-content-center display-1 text-primary p-5" />) : ({ children })
 }
 
 export default UserRouter
