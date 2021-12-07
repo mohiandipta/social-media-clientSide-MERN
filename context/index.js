56e84bcd974b2d925723d42fedc952980d7f4298
mohiandipta <mohianmostofa@gmail.com>
2021-12-07 20:57:00 +0600

    axios interceptor working successfully, automaticaly logout and redirected to the login page by this after jwt expires

--- context/index.js
+++ context/index.js
@@ -1,7 +1,11 @@
 import { useState, createContext, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+
 
 const UserContext = createContext();
 const UserProvider = ({ children }) => {
+    const route = useRouter();
     const [state, setState] = useState({
         user: {},
         token: ""
@@ -10,6 +14,23 @@ const UserProvider = ({ children }) => {
     useEffect(() => {
         setState(JSON.parse(window.localStorage.getItem('auth')))
     }, []);
+
+    // Add a response interceptor
+    axios.interceptors.response.use(
+        function (response) {
+            // Any status code that lie within the range of 2xx cause this function to trigger
+            // Do something with response data
+            return response;
+        }, function (error) {
+            // Any status codes that falls outside the range of 2xx cause this function to trigger
+            // Do something with response error
+            let res = error.response
+            if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+                setState(null)
+                window.localStorage.removeItem("auth")
+                route.push("/login") // redirect to the login page
+            }
+        });
 
     return (
         <UserContext.Provider value={[state, setState]}>
